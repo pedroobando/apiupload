829de291237422e82a421562f769685462c77bf5
pedro obando <pedroobando@hotmail.com>
2020-05-20 18:46:16 -0400

    Moviendo archivo html

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,17 @@
 // Importing routes
 import uploadRoutes from './upload.route';
-// import express from 'express';
+import express from 'express';
 // import path from 'path';
 
 // import htmlIndex from '../index.html';
 
 // import from 'express';
-// import path from 'path';
+import path from 'path';
 
-const rutaStatic = path.join(__dirname,'./html');
+
+const rutaFileStatic = path.join(__dirname,'../../public/uploads');
+
+const rutaRaizStatic = path.join(__dirname,'../html');
 // const rutaStatic = __dirname + '../public/uploads';
 // console.log(rutaStatic);
 
@@ -16,20 +19,18 @@ export default function (app) {
   // routes apps
   app.use('/api/upload', uploadRoutes);
 
-  // app.use('/public', );
-  
-  app.get('/', (req, res) => {
-    res.sendFile('/archivo.html', {root:__dirname});
-  });
-
-  app.use('/raiz',express.static(rutaStatic));
-  // app.get('/raiz', (req, res) => {
-  //   res.sendFile('./html/index.html', {root:__dirname});
+  // app.get('/', (req, res) => {
+  //   res.sendFile('/archivo.html', {root:__dirname});
   // });
 
+  console.log(rutaRaizStatic);
+  console.log(rutaFileStatic);
+
+  app.use('/', express.static(rutaRaizStatic));
+  app.use('/public', express.static(rutaFileStatic));
 
   app.use(function(req, res, next) {
-    res.status(404).json({data:{msg: `Error 404 - No logro encontrar la ruta`});
+    res.status(404).json({data:{msg: `Error 404 - No logro encontrar la ruta`}});
   })  
   
 }
